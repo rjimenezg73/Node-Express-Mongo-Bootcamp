f0d0b1ea759e5dc5eb384133ddb00c6bee2747e2
Roberto Jimenez <robertojimenezg@gmail.com>
2022-12-06 15:03:57 -0600

    File System input - output

--- intro-node/intro.js
+++ intro-node/intro.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSysc('./input.txt', 'utf-8');
+const textIn = fs.readFileSync('./input.txt', 'utf-8');
 console.log(textIn);
+
+const textOut = `This is what we know about avocado: ${textIn}\nCreated on ${Date.now()}`;
+fs.writeFileSync('./output.txt', textOut);
+console.log(textOut);
