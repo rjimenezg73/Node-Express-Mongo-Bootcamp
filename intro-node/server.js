7965cbb955c3d1000bb2776ec1eb4cf9d9a1e6b2
Roberto Jimenez <robertojimenezg@gmail.com>
2022-12-07 00:20:28 -0600

    Creando un servidor http simple

---
+++ intro-node/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// La primera parte es crear el servivor
+const server = http.createServer((req, res) => {
+  res.send('Hello from the Server!!');
+});
+
+// La segunda parte es hacer que el servidor creado escuche las peticiones realizadas
+server.listen(8000, '127.0.0.1', () => {
+  console.clear();
+  console.log('Listening to request on port 8000... 👌');
+});
